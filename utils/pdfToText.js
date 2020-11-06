747f25be9e4d84394f4e33a844f8c0c355444fb5
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-11-06 16:07:52 -0300

    added pdfToText function

---
+++ utils/pdfToText.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
+const pdfToTxt = (pathToPdfFile) => {
+  return new Promise((resolve, reject) => {
+    const dataBuffer = fs.readFileSync(pathToPdfFile);
+    let textContent;
+    pdf(dataBuffer).then((txt) => {
+      resolve(txt.text);
+    });
+  });
+};
+
+module.exports = pdfToTxt;
