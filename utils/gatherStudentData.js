0957005ea5c78b6d7030031ab53006e2749f1608
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-11-08 14:45:48 -0300

    adjusted function for gathering block data

--- utils/gatherStudentData.js
+++ utils/gatherStudentData.js
@@ -2,7 +2,15 @@ const findData = require('./findData');
 const regexStore = require('./regexStore');
 
 const gatherStudentData = (string) => {
-  let object = {};
+  let block = string
+    .replace(/BLOCO II/, 'BLOCO 2')
+    .replace(/BLOCO I/, 'BLOCO 1')
+    .match(/BLOCO ([1-2])/g)
+    .map((el) => el.replace('BLOCO ', ''));
+
+  let object = {
+    block,
+  };
 
   for (key in regexStore) {
     object[key] = findData(string, regexStore[key]);
