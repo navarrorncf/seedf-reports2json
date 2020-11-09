2f4f2f4782ac2238c437fe585b421ff3289241ae
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-11-09 15:49:31 -0300

    added try catch block and return in case of error for ignoring unusable string parts

--- utils/gatherStudentData.js
+++ utils/gatherStudentData.js
@@ -5,22 +5,28 @@ const findData = require('./findData');
 const gatherGradesAttendance = require('./gatherGradesAttendance');
 
 const gatherStudentData = (string) => {
-  let block = string
-    .replace(/BLOCO II/, 'BLOCO 2')
-    .replace(/BLOCO I/, 'BLOCO 1')
-    .match(/BLOCO ([1-2])/g)
-    .map((el) => el.replace('BLOCO ', ''));
+  try {
+    let block = string
+      .replace(/BLOCO II/, 'BLOCO 2')
+      .replace(/BLOCO I/, 'BLOCO 1')
+      .match(/BLOCO ([1-2])/g)
+      .map((el) => el.replace('BLOCO ', ''));
 
-  let object = {
-    block,
-    grades: gatherGradesAttendance(cleanString(string)),
-  };
+    let object = {
+      block,
+      grades: gatherGradesAttendance(cleanString(string)),
+    };
 
-  for (key in regexStore) {
-    object[key] = findData(string, regexStore[key]);
+    for (key in regexStore) {
+      object[key] = findData(string, regexStore[key]);
+    }
+
+    return object;
+  } catch (e) {
+    console.log('EITA!!');
   }
 
-  return object;
+  return {};
 };
 
 module.exports = gatherStudentData;
