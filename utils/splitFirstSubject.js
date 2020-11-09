74a1335023703fff6e30f1dc9e0864e588d3726d
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-11-09 15:51:05 -0300

    added empty string return in case of no match

--- utils/splitFirstSubject.js
+++ utils/splitFirstSubject.js
@@ -1,6 +1,11 @@
 // TODO: Add documentation
 const splitFirstSubject = (str) => {
   const match = str.match(/(COD).*?([0-9]+)(.*)\n/);
+
+  if (!match) {
+    return '';
+  }
+
   return str.replace(match[0], `${match[1]}: ${match[2]}\n${match[3]}\n`);
 };
 
