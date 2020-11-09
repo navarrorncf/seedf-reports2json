82e8323878c62e200d4d3d8e60d4b81aae11b83d
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-11-09 15:54:18 -0300

    refactored error handling

--- utils/gatherStudentData.js
+++ utils/gatherStudentData.js
@@ -23,7 +23,8 @@ const gatherStudentData = (string) => {
 
     return object;
   } catch (e) {
-    console.log('EITA!!');
+    console.log('No Student data found in this part of the file!');
+    console.log(e);
   }
 
   return {};
