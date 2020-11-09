a2baa0f4f26bebe2860e89bc791de765b3228e95
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-11-09 15:50:12 -0300

    added new functions to exports

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,7 @@
+module.exports.chainPromises = require('./chainPromises');
+module.exports.checkFiles = require('./checkFiles');
+module.exports.gatherStudentData = require('./gatherStudentData');
 module.exports.pdfToText = require('./pdfToText');
 module.exports.splitFirstSubject = require('./splitFirstSubject');
 module.exports.splitText = require('./splitText');
+module.exports.storeData = require('./storeData');
