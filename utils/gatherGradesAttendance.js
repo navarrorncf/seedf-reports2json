dfedb7b3fcd4708ac0f27676978b90615414c0a3
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-11-09 10:53:09 -0300

    completed gatherGradesAttendance function missint documentation

--- utils/gatherGradesAttendance.js
+++ utils/gatherGradesAttendance.js
@@ -1,38 +1,36 @@
 const { shorten } = require('./abbreviations');
 
-const dummyText = require('./splitFirstSubject')(
-  require('../dev_data/sampleStudentReport')
-);
-const cleanString = require('./cleanString')(dummyText);
+const gatherGradesAttendance = (string) => {
+  const cleanArray = string.split('\n').filter((el) => el !== '');
 
-const cleanArray = cleanString.split('\n').filter((el) => el !== '');
+  const subjects = cleanArray
+    .filter((el) => el.length > 1 && isNaN(el))
+    .map((el) => shorten[el]);
 
-const subjects = cleanArray
-  .filter((el) => el.length > 1 && isNaN(el))
-  .map((el) => shorten[el]);
+  const splitSubjects = string
+    .split(/[^0-9\-]+\n/)
+    .filter((el) => el !== '')
+    .map((el) => el.split('\n'))
+    .map((el) => el.slice(0, 4));
 
-let splitSubjects = cleanString
-  .split(/[^0-9\-]+\n/)
-  .filter((el) => el !== '')
-  .map((el) => el.split('\n'))
-  .map((el) => el.slice(0, 4));
+  const subjectsArray = subjects
+    .map((el, i) => ({
+      [el]: splitSubjects[i],
+    }))
+    .filter(
+      (el) => !/ENR/.test(Object.keys(el)[0]) && !/PD4/.test(Object.keys(el)[0])
+    );
 
-const subjectsArray = subjects
-  .map((el, i) => ({
-    [el]: splitSubjects[i],
-  }))
-  .filter(
-    (el) => !/ENR/.test(Object.keys(el)[0]) && !/PD4/.test(Object.keys(el)[0])
-  );
+  const subjectsObject = subjectsArray.reduce((acc, cur) => {
+    let key = Object.keys(cur)[0];
+    if (acc[key]) {
+      return Object.assign(acc, { [key]: acc[key].concat(cur[key]) });
+    } else {
+      return Object.assign(acc, cur);
+    }
+  }, {});
 
-const subjectsObject = subjectsArray.reduce((acc, cur) => {
-  let key = Object.keys(cur)[0];
-  if (acc[key]) {
-    return Object.assign(acc, { [key]: acc[key].concat(cur[key]) });
-  } else {
-    return Object.assign(acc, cur);
-  }
-}, {});
+  return subjectsObject;
+};
 
-const gatherGradesAttendance
-console.log(subjectsObject);
+module.exports = gatherGradesAttendance;
