80d59355c16444cd9a7ceb0dd852b9b5d1bc7c3f
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-01-12 20:12:32 -0300

    added default return of empty string
    missing data fields were preventing report card from being stored

--- utils/findData.js
+++ utils/findData.js
@@ -1,11 +1,14 @@
 const findData = (string, regex) => {
+  let data = '';
   try {
-    return string.match(regex)[1];
+    data = string.match(regex)[1];
   } catch (e) {
     if (/Cannot read property/.test(e.stack)) {
       const msg = `Could not find a match for regex ${regex}\nPlease, check your code and/or your pdf file`;
       throw new ReferenceError(msg);
     }
+  } finally {
+    return data;
   }
 };
 
